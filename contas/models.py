54b29e6be7948e608ceb108919c6de1d00d7ffcc
André Santos <andredupmd@gmail>
2021-09-03 16:58:19 -0300

    Add: first model

--- contas/models.py
+++ contas/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.db.models.fields import CharField, EmailField
 
-# Create your models here.
+class Categoria(models.Model):
+
+    nome = models.CharField(max_length=100)
+    data_criacao = models.DateField(auto_now_add=True)
+    
--- contas/views.py
+++ contas/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+import datetime
 
+from django.utils.timezone import now
 
-def contas(request):
-
+def home(request):
+   now = datetime.datetime.now()
    return render(request, 'templates/home.html')
