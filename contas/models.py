d4c568be26594e02b6eedb2018bc7e03d6a3e4ca
André Santos <andredupmd@gmail>
2021-09-03 19:36:47 -0300

    Add: frame for CRUD/'R' Allright

--- contas/models.py
+++ contas/models.py
@@ -14,7 +14,7 @@ class Transacao(models.Model):
     data = models.DateTimeField()
     descricao = models.CharField(max_length= 200)
     valor = models.DecimalField(max_digits= 7, decimal_places=2)
-    categora = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    categoria = models.ForeignKey(Categoria, on_delete = models.CASCADE)
     observacoes = models.TextField(null=True, blank=True)
     class Meta:
         verbose_name_plural = 'Transações'
